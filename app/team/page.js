3d50a264f9104dc9d8c93c608b6ac942c75bfe38
Tanishq Sharma <tanishq22@iiserb.ac.in>
2024-09-15 00:13:19 +0530

    added team page profiles

--- app/team/page.js
+++ app/team/page.js
@@ -5,6 +5,14 @@ import Tile from "@/components/Tile";
 import Image from "next/image";
 import { team } from "@/data/teamData";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+
+
+
 export default function Team() {
     return (
         <>
@@ -17,15 +25,15 @@ export default function Team() {
                 </section>
 
 
-                <div className="w-full flex flex-col items-start p-8 lg:p-24 gap-2  justify-center min-h-screen ">
+                <div className="w-full flex flex-col items-start p-3 lg:p-24 gap-2  justify-center min-h-screen ">
                     <div className="flex items-center justify-center w-full gap-4 mt-24 ">
                         <h1 className="text-4xl lg:text-6xl z-[999] bg-gradient-to-tr from-blue-200 to-blue-100 bg-clip-text text-transparent text-center font-medium p-2 ">We are the people that <br />make up <span className="text-blue-500">CNC.</span> </h1>
                     </div>
 
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 lg:gap-12 mt-12 ">
-                        {team.map((teamMember => (
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-8 gap-x-2 lg:gap-12 mt-12 ">
+                        {team.map(((teamMember, id) => (
                             <TeamMember
-                                key={teamMember.name}
+                                key={id}
                                 name={teamMember.name}
                                 bio={teamMember.bio}
                                 designation={teamMember.designation}
@@ -51,24 +59,99 @@ export default function Team() {
 
 
 
+// const TeamMember = ({ name, key, bio, social_links, designation, profile }) => {
+
+//     const [dot, setDot] = useState(false)
+
+//     return (
+//         <div
+//             key={key}
+//             className="lg:p-6">
+//             <div className="w-fit relative rounded-xl  group transition duration-500 overflow-hidden">
+//                 <img
+
+//                     alt={name}
+//                     src={profile}
+//                     className="aspect-square object-cover group-hover:scale-110 transition duration-500 rounded-xl"
+//                 />
+//                 <div className={`${dot ? "h-fit" : "h-[30px] lg:h-[40px]"} transition duration-500 overflow-hidden bg-blue-900/10 bg-blend-darken rounded-lg backdrop-blur-lg p-1 lg:p-2  gap-3 absolute right-2 lg:right-5 shadow-lg top-2 lg:top-5 items-center`}>
+
+//                     <BsThreeDots className="cursor-pointer md:size-[25px] size-[20px] drop-shadow-lg "
+//                         onClick={() => setDot(!dot)}
+//                     />
+//                     <BiLogoGmail className="cursor-pointer drop-shadow-lg mt-2 md:size-[25px] size-[20px]" />
+//                     <FaLinkedinIn className="cursor-pointer drop-shadow-lg mt-2 md:size-[25px] size-[20px]" />
+//                     <FaGithub className="cursor-pointer drop-shadow-lg mt-2 md:size-[25px] size-[20px]" />
+
+//                 </div>
+//             </div>
+//             <div className="mt-4 flex items-center justify-between">
+//                 <div>
+//                     <h1 className="text-xl lg:text-2xl font-bold mt-2">{name}</h1>
+//                     <h1 className="text-md md:text-lg text-neutral-300  mt-1">{designation}</h1>
+//                 </div>
+
+//             </div>
+//             <h1 className="font-thin text-xs md:text-sm text-neutral-400 mt-4">{bio}</h1>
+
+//         </div>
+//     )
+// }
+
+
+const variants = {
+    open: { opacity: 1 },
+    closed: { opacity: 0.7 },
+}
+
 const TeamMember = ({ name, bio, social_links, designation, profile }) => {
+
+    const [dot, setDot] = useState(false);
+
     return (
-        <div className="lg:p-6   ">
-            <div className="border w-fit">
+        <div className="lg:p-6">
+            <div className="w-fit relative rounded-xl grid place-items-center justify-center group transition duration-500 overflow-hidden">
                 <img
+                    alt={name}
                     src={profile}
-                    className=" rounded-xl"
+                    className="aspect-square object-cover z-[0] group-hover:scale-110 relative transition duration-500 rounded-xl"
                 />
+
+                <motion.div
+                    // whileHover={{ opacity: 1 }}
+                    // initial={{ opacity: 0 }}
+                    animate={dot ? "open" : "closed"}
+                    variants={variants}
+                    transition={{ duration: 0.5 }}
+                    className="transition h-[95%] w-[95%] border overflow-hidden bg-blue-900/10  rounded-lg backdrop-blur-lg p-1 lg:p-2 gap-3 absolute  shadow-xl absolute flex justify-center  items-center "
+                >
+                    <BsThreeDots
+                        className="cursor-pointer absolute right-2 top-2 md:size-[25px] size-[20px] drop-shadow-lg"
+                        onClick={() => setDot(dot => !dot)}
+                    />
+                    <Links />
+
+                </motion.div>
+
             </div>
             <div className="mt-4 flex items-center justify-between">
                 <div>
                     <h1 className="text-xl lg:text-2xl font-bold mt-2">{name}</h1>
-                    <h1 className="text-lg text-neutral-300 lg:text-lg mt-1">{designation}</h1>
+                    <h1 className="text-md md:text-lg text-neutral-300 mt-1">{designation}</h1>
                 </div>
-
             </div>
-            <h1 className="font-thin text-sm text-neutral-400 mt-4">{bio}</h1>
+            <h1 className="font-thin text-xs md:text-sm text-neutral-400 mt-4">{bio}</h1>
+        </div>
+    );
+}
 
+
+const Links = () => {
+    return (
+        <div className="flex gap-4 items-center justify-center ">
+            <BiLogoGmail className="cursor-pointer drop-shadow-lg mt-2 md:size-[25px] size-[20px]" />
+            <FaLinkedinIn className="cursor-pointer drop-shadow-lg mt-2 md:size-[25px] size-[20px]" />
+            <FaGithub className="cursor-pointer drop-shadow-lg mt-2 md:size-[25px] size-[20px]" />
         </div>
     )
 }
--- data/teamData.js
+++ data/teamData.js
@@ -1,190 +1,167 @@
 export const team = [
-    {
-      "name": "John Doe",
-      "bio": "Passionate about tech and organizing council activities.",
-      "profile_picture": "https://github.com/identicons/johndoe.png",
-      "social_links": {
-        "gmail": "john.doe@gmail.com",
-        "github": "https://github.com/johndoe",
-        "linkedin": "https://linkedin.com/in/johndoe"
-      },
-      "designation": "Secretary"
+
+
+  {
+    "name": "Shailesh Kachhi",
+    "bio": "Passionate about creating solutions and working toward societal betterment.",
+    "profile_picture": "team/shailesh.jpg",
+    "social_links": {
+      "gmail": "shaileshkachhi786@gmail.com",
+      "github": "shailesh2290",
+      "linkedin": "https://www.linkedin.com/in/shailesh-kachhi-681a041a2"
     },
-    {
-      "name": "Jane Smith",
-      "bio": "Tech enthusiast and team leader.",
-      "profile_picture": "https://github.com/identicons/janesmith.png",
-      "social_links": {
-        "gmail": "jane.smith@gmail.com",
-        "github": "https://github.com/janesmith",
-        "linkedin": "https://linkedin.com/in/janesmith"
-      },
-      "designation": "Vice Secretary"
+    "designation": "Secretary, CNC"
+  },
+
+  {
+    "name": "Tanishq Sharma",
+    "bio": "Music and programming are the reason I live.",
+    "profile_picture": "team/tanishqsharma.jpg",
+    "social_links": {
+      "gmail": "tanishq22@iiserb.ac.in",
+      "github": "https://www.github.com/tanishqsharma2022",
+      "linkedin": "https://www.linkedin.com/in/tanishq-sharma-iiser/"
     },
-    {
-      "name": "Michael Lee",
-      "bio": "Focused on innovation and team coordination.",
-      "profile_picture": "https://github.com/identicons/michaellee.png",
-      "social_links": {
-        "gmail": "michael.lee@gmail.com",
-        "github": "https://github.com/michaellee",
-        "linkedin": "https://linkedin.com/in/michaellee"
-      },
-      "designation": "Vice Secretary"
+    "designation": "Senior Developer, Developer's Team"
+  },
+  {
+    "name": "Shivajee Kumar",
+    "bio": "Senior Developer with expertise in PHP and SQL (Backend Development).",
+    "profile_picture": "team/shivajeekumar.jpg",
+    "social_links": {
+      "gmail": "shivajee24@iiserb.ac.in",
+      "github": "https://github.com/krshivajee",
+      "linkedin": "www.linkedin.com/in/shivajee24"
     },
-    {
-      "name": "Emily Davis",
-      "bio": "Driven by tech solutions and problem-solving.",
-      "profile_picture": "https://github.com/identicons/emilydavis.png",
-      "social_links": {
-        "gmail": "emily.davis@gmail.com",
-        "github": "https://github.com/emilydavis",
-        "linkedin": "https://linkedin.com/in/emilydavis"
-      },
-      "designation": "Coordinator, Tech Club"
+    "designation": "Senior Developer, Developer's Team"
+  },
+  {
+    "name": "Jay Bharambe",
+    "bio": "🙂",
+    "profile_picture": "team/jaybharambe.jpg",
+    "social_links": {
+      "gmail": "jayb22@iiserb.ac.in",
+      "github": "https://github.com/jayb71",
+      "linkedin": "https://in.linkedin.com/in/jayb71"
     },
-    {
-      "name": "Chris Johnson",
-      "bio": "Passionate about coding and innovation.",
-      "profile_picture": "https://github.com/identicons/chrisjohnson.png",
-      "social_links": {
-        "gmail": "chris.johnson@gmail.com",
-        "github": "https://github.com/chrisjohnson",
-        "linkedin": "https://linkedin.com/in/chrisjohnson"
-      },
-      "designation": "Coordinator, Tech Club"
+    "designation": "Senior Developer, Developer's Team"
+  },
+  {
+    "name": "Adheesh Trivedi",
+    "bio": "I am from EECS department 26'BS.",
+    "profile_picture": "team/adheesh.jpg",
+    "social_links": {
+      "gmail": "adheesh22@iiserb.ac.in",
+      "github": "https://github.com/AdhTri001",
+      "linkedin": "https://www.linkedin.com/in/adheesh-trivedi-8362b318b/"
     },
-    {
-      "name": "Sophia Brown",
-      "bio": "Loves creative design and visual storytelling.",
-      "profile_picture": "https://github.com/identicons/sophiabrown.png",
-      "social_links": {
-        "gmail": "sophia.brown@gmail.com",
-        "github": "https://github.com/sophiabrown",
-        "linkedin": "https://linkedin.com/in/sophiabrown"
-      },
-      "designation": "Coordinator, Design Club"
+    "designation": "Coordinator, Coding Club"
+  },
+  {
+    "name": "Nakshatra Goswami",
+    "bio": "Loves integrating AI models into Financial Data.",
+    "profile_picture": "team/nakshatra.jpg",
+    "social_links": {
+      "gmail": "nakshatra20@iiserb.ac.in",
+      "github": "https://github.com/nakshatra108",
+      "linkedin": "https://www.linkedin.com/in/nakshatra-goswami-02a508203"
     },
-    {
-      "name": "Liam Wilson",
-      "bio": "Innovative designer with a flair for UX.",
-      "profile_picture": "https://github.com/identicons/liamwilson.png",
-      "social_links": {
-        "gmail": "liam.wilson@gmail.com",
-        "github": "https://github.com/liamwilson",
-        "linkedin": "https://linkedin.com/in/liamwilson"
-      },
-      "designation": "Coordinator, Design Club"
+    "designation": "Coordinator, Quant Finance Club"
+  },
+  {
+    "name": "Ananya Gandhi",
+    "bio": "Instantiated for exceptions, deceptions, and infinite loops to isolations.",
+    "profile_picture": "team/ananyagandhi.jpg",
+    "social_links": {
+      "gmail": "ananya.gandhi.183@gmail.com",
+      "github": "https://github.com/ananya183",
+      "linkedin": "https://www.linkedin.com/in/ananya183/"
     },
-    {
-      "name": "Olivia Martinez",
-      "bio": "Focused on growth and leadership.",
-      "profile_picture": "https://github.com/identicons/oliviamartinez.png",
-      "social_links": {
-        "gmail": "olivia.martinez@gmail.com",
-        "github": "https://github.com/oliviamartinez",
-        "linkedin": "https://linkedin.com/in/oliviamartinez"
-      },
-      "designation": "Core Member"
+    "designation": "Founder; Coordinator, Quant Finance Club"
+  },
+
+  {
+    "name": "Bhumika Upadhyay",
+    "bio": "Fascinated by the beauty of art and design and driven by the maths behind coding.",
+    "profile_picture": "team/bhumika.jpg",
+    "social_links": {
+      "gmail": "bhumika22@iiserb.ac.in",
+      "linkedin": "https://www.linkedin.com/in/bhumika-upadhyay-9513722ab"
     },
-    {
-      "name": "James Taylor",
-      "bio": "Tech lead with a passion for collaboration.",
-      "profile_picture": "https://github.com/identicons/jamestaylor.png",
-      "social_links": {
-        "gmail": "james.taylor@gmail.com",
-        "github": "https://github.com/jamestaylor",
-        "linkedin": "https://linkedin.com/in/jamestaylor"
-      },
-      "designation": "Core Member"
+    "designation": "Coordinator, EECS Club"
+  },
+  ,
+  {
+    "name": "Arijit Mondal",
+    "bio": "Driven individual who focuses on results and works under pressure.",
+    "profile_picture": "team/arijitmondal.jpg",
+    "social_links": {
+      "gmail": "arijit22@iiserb.ac.in",
+      "github": "https://github.com/Arijit20031",
+      "linkedin": "https://www.linkedin.com/in/arijit-mandal-081361249"
     },
-    {
-      "name": "Ava Anderson",
-      "bio": "Organized and solution-oriented.",
-      "profile_picture": "https://github.com/identicons/avaanderson.png",
-      "social_links": {
-        "gmail": "ava.anderson@gmail.com",
-        "github": "https://github.com/avaanderson",
-        "linkedin": "https://linkedin.com/in/avaanderson"
-      },
-      "designation": "Core Member"
+    "designation": "Team Member, EECS Club"
+  },
+  {
+    "name": "Prakhar Chandra",
+    "bio": "Constantly curious, always learning, and aiming to grow a little more every day.",
+    "profile_picture": "team/prakharchandra.jpg",
+    "social_links": {
+      "gmail": "prakhar23@iiserb.ac.in",
+      "linkedin": "www.linkedin.com/in/prakhar-chandra-71171028b"
     },
-    {
-      "name": "Ethan Thomas",
-      "bio": "Experienced in tech strategy and leadership.",
-      "profile_picture": "https://github.com/identicons/ethanthomas.png",
-      "social_links": {
-        "gmail": "ethan.thomas@gmail.com",
-        "github": "https://github.com/ethanthomas",
-        "linkedin": "https://linkedin.com/in/ethanthomas"
-      },
-      "designation": "Core Member"
+    "designation": "Marketing Manager, Core Committee"
+  },
+  {
+    "name": "Sumer Singh",
+    "bio": "A passionate and adaptable individual, always eager to embrace new challenges.",
+    "profile_picture": "team/sumersingh.jpg",
+    "social_links": {
+      "gmail": "sumer23@iiserb.ac.in",
+      "github": "Summer193-hash"
     },
-    {
-      "name": "Isabella Moore",
-      "bio": "Creative thinker with a strategic mindset.",
-      "profile_picture": "https://github.com/identicons/isabellamoore.png",
-      "social_links": {
-        "gmail": "isabella.moore@gmail.com",
-        "github": "https://github.com/isabellamoore",
-        "linkedin": "https://linkedin.com/in/isabellamoore"
-      },
-      "designation": "Core Member"
+    "designation": "Execution Manager, Core Committee"
+  },
+  {
+    "name": "Yash Sen",
+    "bio": "My passion is photography and aim is graphic designer.",
+    "profile_picture": "team/yashsen.jpg",
+    "social_links": {
+      "gmail": "sen23@iiserb.ac.in",
+      "linkedin": "https://www.linkedin.com/in/yash-sen-692a032a9"
     },
-    {
-      "name": "Lucas White",
-      "bio": "Tech-savvy with an eye for detail.",
-      "profile_picture": "https://github.com/identicons/lucaswhite.png",
-      "social_links": {
-        "gmail": "lucas.white@gmail.com",
-        "github": "https://github.com/lucaswhite",
-        "linkedin": "https://linkedin.com/in/lucaswhite"
-      },
-      "designation": "Core Member"
+    "designation": "Photography Club Member"
+  },
+  {
+    "name": "Arpan Jain",
+    "bio": "Tech is Life, Life is Tech",
+    "profile_picture": "team/arpanjain.jpg",
+    "social_links": {
+      "gmail": "arpan22@iiserb.ac.in",
+      "github": "https://github.com/Arpan2307",
+      "linkedin": "linkedin.com/in/arpan-jain-iiserb"
     },
-    {
-      "name": "Mia Harris",
-      "bio": "Innovation-driven and team-focused.",
-      "profile_picture": "https://github.com/identicons/miaharris.png",
-      "social_links": {
-        "gmail": "mia.harris@gmail.com",
-        "github": "https://github.com/miaharris",
-        "linkedin": "https://linkedin.com/in/miaharris"
-      },
-      "designation": "Core Member"
+    "designation": "Core Committee Member"
+  },
+  {
+    "name": "Divyam Sood",
+    "bio": "Here be dragons",
+    "profile_picture": "team/divyamsood.jpg",
+    "social_links": {
+      "gmail": "divyam23@iiserb.ac.in",
+      "linkedin": "https://in.linkedin.com/in/divyam-sood-b4618a284"
     },
-    {
-      "name": "Noah Clark",
-      "bio": "Focused on development and collaboration.",
-      "profile_picture": "https://github.com/identicons/noahclark.png",
-      "social_links": {
-        "gmail": "noah.clark@gmail.com",
-        "github": "https://github.com/noahclark",
-        "linkedin": "https://linkedin.com/in/noahclark"
-      },
-      "designation": "Core Member"
+    "designation": "Member, EECS Club"
+  },
+  {
+    "name": "Shubham Mohanty",
+    "bio": "In a matrix of thoughts, I roam, debugging life, making it home.",
+    "profile_picture": "team/shubhammohanty.jpg",
+    "social_links": {
+      "gmail": "shubhamm23@iiserb.ac.in",
+      "github": "https://github.com/shubhammohanty",
+      "linkedin": "www.linkedin.com/in/shubhammohanty"
     },
-    {
-      "name": "Zoe King",
-      "bio": "Driven by curiosity and tech solutions.",
-      "profile_picture": "https://github.com/identicons/zoeking.png",
-      "social_links": {
-        "gmail": "zoe.king@gmail.com",
-        "github": "https://github.com/zoeking",
-        "linkedin": "https://linkedin.com/in/zoeking"
-      },
-      "designation": "Core Member"
-    },
-    {
-      "name": "Henry Scott",
-      "bio": "Tech strategist with a focus on results.",
-      "profile_picture": "https://github.com/identicons/henryscott.png",
-      "social_links": {
-        "gmail": "henry.scott@gmail.com",
-        "github": "https://github.com/henryscott",
-        "linkedin": "https://linkedin.com/in/henryscott"
-      },
-      "designation": "Core Member"
-    }
-  ]
-  
+    "designation": "Market Researcher, Core Committee"
+  },
+];
